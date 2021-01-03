bd272e43d4c8ae4f9e9a1daf45aed956d52b2c57
Shahriya Siddique <ps.shahriya@gmail.com>
2021-01-03 22:58:21 +0600

    initial commit

---
+++ src/Components/HomeContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomeContainer() {
+  return (
+    <div>
+      <h1>Number of cakes</h1>
+      <button onClick={() => alert("hellos")}>Click</button>
+    </div>
+  );
+}
+
+export default HomeContainer;
